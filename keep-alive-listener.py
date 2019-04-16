578f763b0b230f9181df1f3d07f530f84976b43e
greyels <greyels@gmail.com>
2019-04-17 01:59:30 +0300

    Keep alive script for Leonid :)

--- keep-alive-listener.py
+++ keep-alive-listener.py
@@ -5,7 +5,7 @@ import sys
 def main():
     ip, udp_port = sys.argv[1], int(sys.argv[2])
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-    sock.bind((UDP_IP, udp_port))
+    sock.bind((ip, udp_port))
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         print("Received message:", data)
